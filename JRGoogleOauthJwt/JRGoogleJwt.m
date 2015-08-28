958358ee8aacf6f9baa0fa2085bf3aa793d37e5f
Julien Regnauld <j.regnauld@gmail.com>
2015-08-27 18:23:38 -0600

    Adding method to get Private key SecKeyRef

--- JRGoogleOauthJwt/JRGoogleJwt.m
+++ JRGoogleOauthJwt/JRGoogleJwt.m
@@ -5,7 +5,8 @@
 //  Created by Julien on 2015-08-22.
 //  Copyright (c) 2015 Julien. All rights reserved.
 //
-
+#import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonCrypto.h>
 #import "JRGoogleJwt.h"
 
 
@@ -36,7 +37,7 @@ static NSString *const kGoogleHeaderJSON = @"{\"alg\":\"RS256\",\"typ\":\"JWT\"}
     self = [super init];
     if (self) {
         _iss = iss;
-        _scope = [scope copy];
+        _scope = scope;
         _privateKeyName = privateKeyName;
         _aud = kGoogleAud;
         NSTimeInterval currentTimeInSec = [[NSDate date] timeIntervalSince1970];
@@ -71,15 +72,53 @@ static NSString *const kGoogleHeaderJSON = @"{\"alg\":\"RS256\",\"typ\":\"JWT\"}
 
 -(void)generateSignedGoogleJwt:(SignedGoogleJwtCompletionBlock)block{
     
-    NSString *signedGoogleJwt = = [NSString
+    NSString *signedGoogleJwt = [NSString
                                    stringWithFormat:@"%@.%@.%@", self.jwtClaimBase64Encoded, self.jwtClaimBase64Encoded,
                                    @""];
     
     block(signedGoogleJwt);
 
 }
-@end
 
+-(SecKeyRef)getPrivateKey{
+
+    NSString *resourcePath =
+    [[NSBundle mainBundle] pathForResource:self.privateKeyName ofType:@"p12"];
+    if(!resourcePath){
+        @throw [NSException exceptionWithName:NSInvalidArgumentException
+                                       reason:@"Please make sure that your private key has been added to the project."
+                                     userInfo:nil];
+    }
+    else {
+        SecKeyRef privateKeyRef = NULL;
+        NSData *p12Data = [NSData dataWithContentsOfFile:resourcePath];
+        if (!p12Data) {
+            return privateKeyRef;
+        }
+        NSMutableDictionary *options = [[NSMutableDictionary alloc] init];
+        [options setObject:kGooglePassPhrasePrivateKey
+                forKey:(__bridge id)kSecImportExportPassphrase];
+    
+        //trick to avoid memory leak in iOS 8.0 and let ARC do the job for you
+        NSArray *test = [[NSArray alloc] init];
+        CFArrayRef items = CFBridgingRetain(test);
+
+        OSStatus securityError = SecPKCS12Import(
+                                             (__bridge CFDataRef)p12Data, (__bridge CFDictionaryRef)options, &items);
+        if (securityError == noErr && CFArrayGetCount(items) > 0) {
+            CFDictionaryRef identityDict = CFArrayGetValueAtIndex(items, 0);
+            SecIdentityRef identityApp = (SecIdentityRef)CFDictionaryGetValue(
+                                                                          identityDict, kSecImportItemIdentity);
+            securityError = SecIdentityCopyPrivateKey(identityApp, &privateKeyRef);
+            if (securityError != noErr) {
+            privateKeyRef = NULL;
+            }
+        }
+        CFBridgingRelease(items);
+        return privateKeyRef;
+    }
+}
+@end
 /**
  * Creates the signature for the passed plain data.
  * Signature is created by encrypting the SHA256 hash value of the plain data.
